be63dabc824ec6934ed33d4a5039c2456e8fbde3
amin <aungkhantmin.dev@gmail.com>
2025-03-25 18:51:23 +0200

    added pause button and fixed the cat's positions

--- make-your-game/demo game/js.js
+++ make-your-game/demo game/js.js
@@ -1,38 +1,60 @@
 const gameBox = document.getElementById("gBox")
+let isPaused = false
 
-function createDuck() {
-    const duck = document.createElement("div");
-    duck.classList.add("duck");
+pauseButton = document.getElementById("pause")
+pauseButton.addEventListener("click", () => {
+    isPaused = !isPaused;
+    pauseButton.textContent = isPaused ? "Resume" : "Pause";
+    if (!isPaused) {
+        requestAnimationFrame(moveCats);
+    }
+});
 
-    const ranNum = Math.random()
-    /* if (ranNum === 0){
+function createCat() {
+    const cat = document.createElement("div");
+    cat.classList.add("cat");
+    gameBox.appendChild(cat);
 
-    } */
-    let topPos = ranNum * (gameBox.clientHeight - duck.clientHeight); // Random Y position
+    let ranNum = Math.random();
+    let topPos = ranNum * (gameBox.clientHeight - cat.clientHeight); 
     
-    duck.style.top = `${topPos}px`;
-    duck.style.left = `${duck.clientWidth}px`; // Start off-screen
-
-    document.body.appendChild(duck);
-
-    // Move duck across the screen
-    let moveDuck = setInterval(() => {
-        let currentLeft = parseInt(duck.style.left);
-        if (currentLeft > gameBox.clientWidth-duck.clientWidth-10) {
-            clearInterval(moveDuck);
-            duck.remove();
-        } else {
-            duck.style.left = `${currentLeft + 5}px`;
-        }
-    }, 30);
+    cat.style.top = `${topPos}px`;
+    cat.style.left = "0px"; 
 
     // Click to "shoot" the duck
-    duck.addEventListener("click", () => {
-        clearInterval(moveDuck);
-        duck.style.transform = "rotate(90deg)"; // Make it look like it falls
-        setTimeout(() => duck.remove(), 500);
+    cat.addEventListener("click", () => {
+        cat.style.transform = "rotate(90deg)"; 
+        setTimeout(() => cat.remove(), 500);
     });
 }
 
-// Spawn a new duck every 2 seconds
-setInterval(createDuck, 5000);
+function moveCats() {
+
+    const cats = document.querySelectorAll(".cat");
+    const maxRight = gameBox.clientWidth - 80;
+
+    cats.forEach((cat) => {
+    let currentLeft = parseInt(cat.style.left);
+
+    if (!isPaused){
+        if (currentLeft < maxRight) {
+            cat.style.left = `${currentLeft + 3}px`;
+        } else {
+            cat.remove();
+        }
+    }})
+    
+    if (!isPaused) {
+        requestAnimationFrame(moveCats);
+    }
+}
+
+function main(){
+    setInterval(() => {
+        if (!isPaused) createCat();
+    }, 2000);
+
+    requestAnimationFrame(moveCats)
+}
+
+main()
